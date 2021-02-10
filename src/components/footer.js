3d0eb0fd8571a85c47d636116a37e826bc7fb581
Guido <carucciguido@gmail.com>
2021-02-10 14:40:48 +0000

    footer

--- src/components/footer.js
+++ src/components/footer.js
@@ -6,32 +6,37 @@ const Footer = () => {
     <div className='footer'>
       <div className='footer-info-main-container'>
         <div className='footer-info-container'>
-          <h3>Forlì-Cesena</h3>
+          <h4>Forlì-Cesena</h4>
           <p className='footer-info'>Via Don Ercole Fiori, 9,
 47042 Cesenatico (FC)
           </p>
-          <p className='footer-info'>+39  349 0887444</p>
-          <p className='footer-info'>info@manciniceronistudiolegale.it</p>
+          {/* <p className='footer-info'>+39  349 0887444</p>
+          <p className='footer-info'>info@manciniceronistudiolegale.it</p> */}
 
         </div>
         <div className='footer-info-container'>
-          <h3>Ancona</h3>
+          <h4>Ancona</h4>
           <p className='footer-info'>Via Alcide De Gasperi, 82, 60125 Ancona (AN)
           </p>
-          <p className='footer-info'>+39  349 0887444	</p>
-          <p className='footer-info'>info@manciniceronistudiolegale.it</p>
+          {/* <p className='footer-info'>+39  349 0887444	</p>
+          <p className='footer-info'>info@manciniceronistudiolegale.it</p> */}
 
         </div>
         <div className='footer-info-container'>
-          <h3>Fermo</h3>
+          <h4>Fermo</h4>
           <p className='footer-info'>Via Campogrande, 21, 63831 Rapagnano (FM)
           </p>
-          <p className='footer-info'>+39  349 0887444	</p>
-          <p className='footer-info'>info@manciniceronistudiolegale.it</p>
+          {/* <p className='footer-info'>+39  349 0887444	</p>
+          <p className='footer-info'>info@manciniceronistudiolegale.it</p> */}
 
         </div>
-
       </div>
+      <div className='contact'>
+        <p><strong>Contatti</strong></p>
+        <p className='footer-info'><a href='+39  349 0887444'>+39  349 0887444</a></p>
+        <p className='footer-info'><a href='mailto:info@manciniceronistudiolegale.it'>info@manciniceronistudiolegale.it</a></p>
+      </div>
+
       <style jsx='true'>{`
          .footer {
              display:block;
@@ -47,11 +52,22 @@ const Footer = () => {
         }
         .footer-info {
             color: var(--grey);
+            max-width: 70%;
+            margin: 0 auto
             
         }
       
         .footer-info a:hover {
             color: var(--main-color)
+        }
+
+        .contact {
+          margin-top: 60px
+        }
+        .footer-info-container h4, .contact strong {
+          position: relative;
+          left: 50px;
+          top: 10px
         }
     
     @media(min-width: 968px){
@@ -68,6 +84,15 @@ const Footer = () => {
              margin: 0 auto;
              display: block
          }
+         .contact {
+          text-align: center;
+          margin-top: 0;
+          position: relative;
+          bottom: 10px
+        }
+        .contact strong {
+          left: 0
+        }
     }
          
          `}
