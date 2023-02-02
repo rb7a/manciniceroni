322eb1bad31ea3935b63ed6d5dcb41d9bdf00ed9
Shubhom <shubhom.juneja@gmail.com>
2023-02-02 18:56:10 +0530

    remove comma from contact page

--- src/pages/contatti.js
+++ src/pages/contatti.js
@@ -81,7 +81,7 @@ export default () => {
             >
               <h3 className='address'>Ancona</h3>
               <p className='contact-address'>60125 ANCONA (AN)</p>
-              <p className='contact-address'>Via Alcide De, Gasperi, 82 </p>
+              <p className='contact-address'>Via Alcide De Gasperi, 82 </p>
               {/* <p className='contact-address'><a href='mailto:info@ceronimancini.it'>info@ceronimancini.it</a></p> */}
 
               <iframe src='https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2888.9697678037182!2d13.507470515302106!3d43.60717107912294!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x132d7f907aa79ad3%3A0x30dcbba1e2cb19cb!2sVia%20Alcide%20de%20Gasperi%2C%2082%2C%2060125%20Ancona%20AN%2C%20Italy!5e0!3m2!1sen!2ses!4v1611057132380!5m2!1sen!2ses' width='100%' height='400px' frameborder='0' allowfullscreen='' aria-hidden='false' tabindex='0' />
