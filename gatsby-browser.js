2ddaa82c31cbabd47dfbdc76d72aa7f7827dcafd
Shubhom <shubhom.juneja@gmail.com>
2023-02-06 17:14:55 +0530

    fixed scroll to top issue on navigation

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,12 +1,13 @@
-require('typeface-poppins')
-  // in gatsby-browser.js
+require("typeface-poppins")
+// in gatsby-browser.js
 export function shouldUpdateScroll(prevRouterProps, { location }) {
-    console.log(prevRouterProps.pathname);
-    if(prevRouterProps.pathname!=='/aree-di-attivita/')
-    {
-    window.scrollTo(0, 0)
-    const body = document.getElementsByTagName('body')[0]
-    body.scrollTop = 0
-    return false
-    }
-  }
+
+  // if(prevRouterProps.pathname!=='/aree-di-attivita/')
+  // {
+  // window.scrollTo(0, 0)
+  const body = document.getElementsByTagName("body")[0]
+  body.scrollTop = 0
+
+  return false
+  // }
+}
